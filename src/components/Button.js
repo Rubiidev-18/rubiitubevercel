2e03d8067b361da898dc5721d1c979260d74c2c6
Rubiidev <158863401+Rubiidev-18@users.noreply.github.com>
2024-04-18 16:42:47 -0700

    button color change

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ name }) => {
-  return <div className="m-2 px-4 py-1 rounded-lg bg-gray-200">{name}</div>;
+  return <div className="m-2 px-4 py-1 rounded-lg bg-red-200">{name}</div>;
 };
 
 export default Button;
