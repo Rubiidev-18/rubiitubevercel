f2055f8e4550e3e87f0d27ea5d5fe67f058a3057
Rubiidev <158863401+Rubiidev-18@users.noreply.github.com>
2024-04-18 16:46:12 -0700

    more accurate to yt layout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,9 +12,8 @@ const Sidebar = () => {
     <div className="p-5 shadow-lg col-span-1 w-40">
       <ul className="mt-2">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/memoization">Memoization</Link></li>
-        <li>Shorts</li>
         <li>Videos</li>
+        <li>Shorts</li>
         <li>Live</li>
       </ul>
       <h1 className="font-bold mt-1">Subscriptions</h1>
@@ -22,14 +21,12 @@ const Sidebar = () => {
         <li>Music</li>
         <li>Sport</li>
         <li>Gaming</li>
-        <li>Movie</li>
       </ul>
       <h1 className="font-bold mt-1">Watch Later</h1>
       <ul>
         <li>Music</li>
         <li>Sport</li>
         <li>Gaming</li>
-        <li>Movie</li>
       </ul>
     </div>
   );
